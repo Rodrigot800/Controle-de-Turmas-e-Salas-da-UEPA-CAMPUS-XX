92c3c9263b6f997eeca2d2ae0b2b10ac68250a48
Rodrigo da Silva Peixoto <SEU_EMAIL_DO_GITHUB@gmail.com>
2026-02-02 17:19:42 -0300

    primeiro commit

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <div className="header-icon">📘</div>
+
+                <div>
+                    <h1>Controle de Salas e Turmas – UEPA</h1>
+                    <span>Sistema acadêmico de organização de salas</span>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
